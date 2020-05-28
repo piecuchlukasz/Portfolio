574670602c4c14d723d471f507b102f891f444cf
LukasP <piecuchlukaszuk@gmail.com>
2020-05-28 22:43:32 +0100

    Updates

---
+++ precache-manifest.33e04ef33649a3b34b88518f95ceba7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f71fa1f7f773b96c242828ec5097c0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0d85ec8b1d9962793939",
+    "url": "/portfolio/static/css/main.a467f683.chunk.css"
+  },
+  {
+    "revision": "bd938ff8660ecf96fd5f",
+    "url": "/portfolio/static/js/2.71f4bc9f.chunk.js"
+  },
+  {
+    "revision": "9a966710451bb787fe70d36430e41259",
+    "url": "/portfolio/static/js/2.71f4bc9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d85ec8b1d9962793939",
+    "url": "/portfolio/static/js/main.7b4b9577.chunk.js"
+  },
+  {
+    "revision": "83f657771b7854dba4c6",
+    "url": "/portfolio/static/js/runtime-main.911f39c4.js"
+  },
+  {
+    "revision": "168a91b480bc177a7c80aa365b32c191",
+    "url": "/portfolio/static/media/backgroud_thequays.168a91b4.jpg"
+  }
+]);
